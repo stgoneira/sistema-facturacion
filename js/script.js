2f939118f4024bfa94306f2a13a61520f9bb58a0
stgoneira <stgoneiran@gmail.com>
2022-02-15 21:08:32 -0300

    Enlaces del menú. Creamos footer. Agregamos carrusel de imgs y una vista de 3 productos en el Inicio.

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// load y DOMContentLoaded 
+window.addEventListener('DOMContentLoaded', (loadEvent) => {
+    $("header").load("trozos-codigo/header.html");
+    $("footer").load("trozos-codigo/footer.html");
+});
+
